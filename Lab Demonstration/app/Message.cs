f83cbc0a4146c5c4e8e80ea55db8eb48b7557f3e
vasudevkv <138557269+vasudevkv@users.noreply.github.com>
2025-08-26 13:08:06 +1000

    Add files via upload

---
+++ Lab Demonstration/app/Message.cs
@@ -0,0 +1,17 @@
+namespace HelloWorld{
+    public class Message{
+        private string _text;
+
+        public Message(string txt){
+            _text = txt;
+        }
+
+        public void Print(){
+            Console.WriteLine(_text);
+        }
+
+        public string GetMessage(){
+            return this._text;
+        }
+    }
+}
---
+++ Lab Demonstration/app/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace HelloWorld
+{
+    class Program{
+        public static void Main(string[] args)
+
+        {
+
+            Message myMessage1 = new Message("Hello - First Message object ID is 104672319");
+            myMessage1.Print();
+
+            Message myMessage2 = new Message("Hello - First Message object ID is 104672319");
+            myMessage2.Print();
+            //
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            string prefix_string = "COS2007";
+            string suffix_string = "Hello1";
+
+            Console.WriteLine(prefix_string + " " + suffix_string);
+
+        }
+    }
+}
+
